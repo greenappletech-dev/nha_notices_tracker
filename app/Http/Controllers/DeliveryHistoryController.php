3105ac1ac93d721ed855b6c13dd851276a720c90
Jose Miguel Bautista <bautista.joseemigueel@gmail.com>
2025-03-03 15:50:21 +0800

    view image in delivery history tab

--- app/Http/Controllers/DeliveryHistoryController.php
+++ app/Http/Controllers/DeliveryHistoryController.php
@@ -12,18 +12,21 @@ class DeliveryHistoryController extends Controller
     {
         $deliveries = Delivery::select(
             'deliveries.id',
+            'deliveries.photo',
             'demand_notice_trackers.name as notice_type_name',
             'projects.name as project_name',
             'beneficiaries.name as beneficiary_name',
             'beneficiaries.address',
             'deliveries.date_captured',
-            'users.name as collector_name',
+            'users.name as collector_name'
         )
         ->leftJoin('demand_notice_trackers', 'deliveries.notice_type_id', 'demand_notice_trackers.id')
         ->leftJoin('projects', 'projects.id', 'deliveries.project_id')
-        ->leftJoin('beneficiaries','beneficiaries.id', 'deliveries.beneficiary_id')
-        ->leftJoin('users', 'users.id', 'deliveries.user_id')->get();
-        
-        return view('deliveryhistory', compact('deliveries' ));
+        ->leftJoin('beneficiaries', 'beneficiaries.id', 'deliveries.beneficiary_id')
+        ->leftJoin('users', 'users.id', 'deliveries.user_id')
+        ->get();
+    
+        return view('deliveryhistory', compact('deliveries'));
     }
+    
 }
